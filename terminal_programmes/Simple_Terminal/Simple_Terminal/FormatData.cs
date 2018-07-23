879fdc81007f3f2d8b165cd31b428b4eaf7126cb
Alexander S. Lepatenko <Lepatenko@BSVT-NT>
2018-07-23 19:21:10 +0300

    Terminal_Programm

---
+++ terminal_programmes/Simple_Terminal/Simple_Terminal/FormatData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Terminal
+{
+   
+    public struct IHH_Formata_Data
+    {
+
+    }
+
+}
